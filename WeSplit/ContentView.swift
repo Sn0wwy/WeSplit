0cf0e7f305ab0fa7be56a8826cc2470304f1ecb3
Ignacio Alavés <nachoalasanchez@gmail.com>
2023-07-17 12:52:16 +0200

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Nacho Alaves on 17/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    Text("Hello, world!")
+                }
+            }
+            .navigationTitle("SwiftUI")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Nacho Alaves on 17/7/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
